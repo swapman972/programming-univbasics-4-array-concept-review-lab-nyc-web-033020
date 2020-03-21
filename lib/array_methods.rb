f12c10c5c17e2b9d6f2687ba7133b24c5ad83d57
Jordan Melidor-Fuxis <jordan.melidor@hotmail.fr>
2020-03-21 22:53:25 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,8 +10,8 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  counter = 0
-  answer = 0
+  counter = 1
+  answer = array[0]
   while array.length > counter do
     if array[counter] > answer
       answer = array[counter]
@@ -22,5 +22,13 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  
+  counter = 1
+  answer = array[0]
+  while array.length > counter do
+    if array[counter] < answer
+      answer = array[counter]
+    end
+    counter += 1
+  end
+  answer
 end
